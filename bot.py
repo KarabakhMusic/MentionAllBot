3e0751c04641893e31c72d17e71eda39e5328ad0
PornoHup <107538335+PornoHup@users.noreply.github.com>
2024-06-01 22:00:50 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,12 +21,12 @@ spam_chats = []
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
   await event.reply(
-    "**Salam 👋 Men @DBMBOSSdu terefinden yaradılmış tağ botuyam**, Qrupda ve Kanallarda Hamını tağ ede bilerem 👻\nBas **/help** daha çox melumatı elde ede bilersiniz",
+    "**Salam 👋 Men 𝐓𝐮𝐫𝐚𝐥 terefinden yaradılmış tağ botuyam**, Qrupda ve Kanallarda Hamını tağ ede bilerem 👻\nBas **/help** daha çox melumatı elde ede bilersiniz",
     link_preview=False,
     buttons=(
       [
-        Button.url('🇦🇿 Qrup', 'https://t.me/Together_Time'),
-        Button.url('⚜️ Sahib', 'https://t.me/DBMBOSSdu')
+        Button.url('💞 Qrup', 'https://t.me/NezrinBot'),
+        Button.url('🇦🇿 Sahib', 'https://t.me/hmnevar')
       ]
     )
   )
@@ -39,8 +39,8 @@ async def help(event):
     link_preview=False,
     buttons=(
       [
-        Button.url('🇦🇿 Qrup', 'https://t.me/Together_Time'),
-        Button.url('⚜️ Sahib', 'https://t.me/DBMBOSSdu')
+        Button.url('💞 Qrup', 'https://t.me/nezrinbot'),
+        Button.url('🇦🇿 Sahib', 'https://t.me/hmnevar')
       ]
     )
   )
